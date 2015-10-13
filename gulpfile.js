e414d17dc767f76aea0763599eb9f72727b5431b
LeeHyungGeun <lhg4031@gmail.com>
2015-10-13 23:55:52 +0800

    remove forever and node-mon

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    forever = require('forever-monitor'),
     browserSync = require('browser-sync').create();
 
 // Static server
@@ -13,8 +12,4 @@ gulp.task('browser-sync', function () {
   browserSync.watch('*.html').on('change', browserSync.reload);
 });
 
-gulp.task('server', function () {
-  new forever.Monitor('gulpfile.js').start();
-});
-
-gulp.task('default', ['browser-sync', 'server']);
+gulp.task('default', ['browser-sync']);
